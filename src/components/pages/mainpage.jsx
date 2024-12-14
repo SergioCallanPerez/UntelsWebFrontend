af104309fe33da1fae9c768b39619a5d58c57558
SergioCallanPerez <sergio.callan45@gmail.com>
2024-12-14 12:07:30 -0500

    Adding file order and folders

---
+++ src/components/pages/mainpage.jsx
@@ -0,0 +1,10 @@
+import {React} from "react"
+export default function MainPage(){
+    return(
+        <main>
+            <body>
+                <p>Hello world</p>
+            </body>
+        </main>
+    )
+}
