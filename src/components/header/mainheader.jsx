88b4497c30cbe8e30491c82896c0ba7c285e3de8
SergioCallanPerez <sergio.callan45@gmail.com>
2024-12-14 12:39:03 -0500

    Adding header

---
+++ src/components/header/mainheader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../assets/logoUntels.png'
+import portal from '../../assets/portalLogo.png'
+
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><a href='/'><img src={logo}/></a></li>
+                    <li><a href='http://transparencia.gob.pe/enlaces/pte_transparencia_enlaces.aspx?id_entidad=13444#.XwbQV21KjIV'><img src={portal}/></a></li>
+                    
+                </ul>
+            </nav>
+        </header>
+    )
+}
--- src/components/pages/mainpage.jsx
+++ src/components/pages/mainpage.jsx
@@ -1,8 +1,10 @@
 import {React} from "react"
+import Header from "../header/mainheader"
 export default function MainPage(){
     return(
         <main>
             <body>
+                <Header/>
                 <p>Hello world</p>
             </body>
         </main>
