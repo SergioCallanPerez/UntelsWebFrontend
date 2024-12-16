260b30ded7e36c6844e4c8f423d996f4c5483de8
SergioCallanPerez <sergio.callan45@gmail.com>
2024-12-16 17:27:41 -0500

    Changing folder location (Assets)

---
+++ src/components/assets/maincarrouseldata.jsx
@@ -0,0 +1,32 @@
+export const mainCarrouselData= [
+    {
+        id: 1, imgUrl: ""
+    },
+    {
+        id: 2, imgUrl: ""
+    },
+    {
+        id: 3, imgUrl: ""
+    },
+    {
+        id: 4, imgUrl: ""
+    },
+    {
+        id: 5, imgUrl: ""
+    },
+    {
+        id: 6, imgUrl: ""
+    },
+    {
+        id: 7, imgUrl: ""
+    },
+    {
+        id: 8, imgUrl: ""
+    },
+    {
+        id: 9, imgUrl: ""
+    },
+    {
+        id: 10, imgUrl: ""
+    }
+]
