0b32ab3bcd4591313875b83fe3633bb09659bf0c
Franco Antonio Matos Ramos <113787760+FrancoAMR@users.noreply.github.com>
2024-12-18 16:03:43 -0500

    Importing Announcements in main index

--- src/main/pages/main/index.jsx
+++ src/main/pages/main/index.jsx
@@ -3,6 +3,7 @@ import Header from "../../components/header";
 import MainCarousel from "./carousel.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/main_index.css'
+import Announcements from "./announcements.jsx"
 
 export default function Main() {
   return (
@@ -10,6 +11,7 @@ export default function Main() {
       <Header />
       <body>
         <MainCarousel />
+        <Announcements/>
         <p>Hello world</p>
       </body>
     </main>
