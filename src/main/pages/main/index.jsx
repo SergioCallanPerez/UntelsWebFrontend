7e6053d6dbdf9de03b04659331b778fae467cfb8
Franco Antonio Matos Ramos <113787760+FrancoAMR@users.noreply.github.com>
2024-12-18 16:46:41 -0500

    Fixing index

--- src/main/pages/main/index.jsx
+++ src/main/pages/main/index.jsx
@@ -2,13 +2,19 @@ import { React } from "react";
 import MainCarousel from "./carousel.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Follow from "./follow.jsx";
+import Announcements from "./announcements.jsx";
+import '../../styles/main_index.css'
+import Header from "../../components/header";
 
 export default function Main() {
   return (
     <main>
-      <MainCarousel />
-      <p>Hello world</p>
-      <Follow />
+      <Header />
+      <body>
+        <MainCarousel />
+        <Announcements />
+        <Follow />
+      </body>
     </main>
   );
 }
