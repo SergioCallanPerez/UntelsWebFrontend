965746b553fd00387bc4c784c57692dd2568d33b
SergioCallanPerez <153020034+SergioCallanPerez@users.noreply.github.com>
2024-12-18 17:48:17 -0500

    Minor fixes in index

--- src/main/pages/main/index.jsx
+++ src/main/pages/main/index.jsx
@@ -4,13 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Follow from "./follow.jsx";
 import Announcements from "./announcements.jsx";
 import '../../styles/main_index.css'
-import Header from "../../components/header";
 import Experiences from "./experiences.jsx";
 
 export default function Main() {
   return (
     <main>
-      <Header />
       <body>
         <MainCarousel />
         <Announcements />
