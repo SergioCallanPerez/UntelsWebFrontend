9b5d5b5c6d084f459c8a6257194209da8032bd0c
Franco Antonio Matos Ramos <113787760+FrancoAMR@users.noreply.github.com>
2024-12-18 13:20:55 -0500

    Creating new section

--- src/main/pages/main/index.jsx
+++ src/main/pages/main/index.jsx
@@ -2,12 +2,14 @@ import { React } from "react";
 import Header from "../../components/header";
 import MainCarousel from "./carousel.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import News from "./news.jsx";
 
 export default function Main() {
   return (
     <main>
       <Header />
       <MainCarousel />
+      <News />
       <p>Hello world</p>
     </main>
   );
