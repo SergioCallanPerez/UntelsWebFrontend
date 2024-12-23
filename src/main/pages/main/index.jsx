08915fed89fa0156776c15c3cfc24d1f8d040e47
JoshuaPariona <joshiparsa@gmail.com>
2024-12-23 00:37:24 -0500

    Moving Bootstrap CSS to root

--- src/main/pages/main/index.jsx
+++ src/main/pages/main/index.jsx
@@ -1,6 +1,5 @@
 import { React } from "react";
 import MainCarousel from "./carousel.jsx"
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Follow from "./follow.jsx";
 import Announcements from "./announcements.jsx";
 import '../../styles/main_index.css'
@@ -8,7 +7,7 @@ import Experiences from "./experiences.jsx";
 
 export default function Main() {
   return (
-    <main>
+    <main className="d-flex flex-column gap-4 pb-4" >
       <MainCarousel />
       <Announcements />
       <Experiences/>
