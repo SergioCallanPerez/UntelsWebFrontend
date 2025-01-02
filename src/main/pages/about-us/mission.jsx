539ec62df3a531f998e618b262f5d77376e9544b
Franco Antonio Matos Ramos <113787760+FrancoAMR@users.noreply.github.com>
2025-01-01 23:44:19 -0500

    Update mission.jsx

--- src/main/pages/about-us/mission.jsx
+++ src/main/pages/about-us/mission.jsx
@@ -6,15 +6,18 @@ import "../../styles/root.css";
 export default function Mission() {
   return (
     <main>
-      <Row>
-        <Row className="primary">
-          <h3>Misión</h3>
+      <Row className="">
+        <Row className="primary ">
+          <h3 className="ms-3">Misión</h3>
+          <p className="ms-3">Formar profesionales, investigadores e innovadores, con base humanista, ética, social, tecnológica y científica. Que participen en la solución de los problemas de la sociedad, con un enfoque de desarrollo sostenible, vocación de servicio y espíritu emprendedor. En el ámbito local, nacional y con proyección internacional.</p>
         </Row>
-        <Row className="background">
-          <h3>Visión</h3>
+        <Row className="background text-end">
+          <h3 className="me-3">Visión</h3>
+          <p className="me-3">Ser reconocida como una Universidad socialmente responsable. Integrado por un equipo humano comprometido y competente en la formación integral de sus estudiantes. Con aporte de innovación hacia la sociedad, mediante centros de investigación y desarrollo, incubadoras de emprendimiento y educación continua.</p>
         </Row>
         <Row className="primary">
-          <h3>Valores</h3>
+          <h3 className="ms-3">Valores</h3>
+          <p className="ms-3">Respeto, libertad, mejora continua, honestidad, solidaridad y justicia, responsabilidad social.</p>
         </Row>
       </Row>
     </main>
