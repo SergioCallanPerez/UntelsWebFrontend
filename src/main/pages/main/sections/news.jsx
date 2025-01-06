fe60c9fadb45ba498d3646527ba5066a86255c4f
JoshuaPariona <joshiparsa@gmail.com>
2025-01-06 14:43:57 -0500

    Refactor news section with responsive design and improved card layout

--- src/main/pages/main/sections/news.jsx
+++ src/main/pages/main/sections/news.jsx
@@ -1,49 +1,87 @@
 import { useState } from "react";
-import Carousel from "react-bootstrap/Carousel";
 
 import "@/styles/news.css";
 import carouselIMG1 from "@/assets/news_assets/new1.png";
 import carouselIMG2 from "@/assets/news_assets/new2.png";
 import carouselIMG3 from "@/assets/news_assets/new3.png";
+import { Card, Container } from "react-bootstrap";
+import PagerView from "@/components/pager";
+import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
-const carouselElements = [
+const news = [
   {
     id: "1",
     img: carouselIMG1,
-    text: "Lorem, ipsum dolor sit amet consectetur",
+    title: "Lorem, ipsum dolor sit amet consectetur",
+    text: "Lorem, ipsum dolor sit amet consectetur, Lorem, ipsum dolor sit amet consectetur",
   },
   {
     id: "2",
     img: carouselIMG2,
-    text: "Lorem, ipsum dolor sit amet consectetur",
+    title: "Lorem, ipsum dolor sit amet consectetur",
+    text: "Lorem, ipsum dolor sit amet consectetur, Lorem, ipsum dolor sit amet consectetur",
   },
   {
     id: "3",
     img: carouselIMG3,
-    text: "Lorem, ipsum dolor sit amet consectetur",
+    title: "Lorem, ipsum dolor sit amet consectetur",
+    text: "Lorem, ipsum dolor sit amet consectetur, Lorem, ipsum dolor sit amet consectetur",
+  },
+  {
+    id: "4",
+    img: carouselIMG3,
+    title: "Lorem, ipsum dolor sit amet consectetur",
+    text: "Lorem, ipsum dolor sit amet consectetur, Lorem, ipsum dolor sit amet consectetur",
+  },
+  {
+    id: "5",
+    img: carouselIMG3,
+    title: "Lorem, ipsum dolor sit amet consectetur",
+    text: "Lorem, ipsum dolor sit amet consectetur, Lorem, ipsum dolor sit amet consectetur",
   },
 ];
 
 export default function News() {
-  const [index, setIndex] = useState(0);
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
+  const isMd = useMediaQuery({
+    query: "(max-width: 768px)",
+  });
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect} className="carousel">
-      {carouselElements.map((element) => (
-        <Carousel.Item key={element.id} interval={4000}>
-          <img
-            src={element.img}
-            alt={`Carrusel ${element.id + 1}`}
-            className="carouselNewIMG"
-          />
-          <Carousel.Caption className="captions">
-            {element.text}
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <section>
+      <Container fluid className="mx-0 px-0 mt-3">
+        <Container className="d-flex flex-column">
+          <h2 className="background-title with-underline text-center">
+            <b>Noticias</b>
+          </h2>
+          <Link className={`background my-4 ${isMd ? "mx-auto" : "ms-auto"}`}>
+            Ver todo
+          </Link>
+          <PagerView onBackground>
+            {news.map((news) => (
+              <Card
+                key={news.id}
+                className="background border-0 rounded-4 mx-4 mb-2 with-box-shadow"
+              >
+                <Card.Img
+                  className="news-cardImg rounded-4"
+                  variant="top"
+                  src={news.img}
+                />
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title as={"h5"} className="mt-3">
+                    {news.title}
+                  </Card.Title>
+                  <Card.Text as={"p"} className="mt-4">
+                    {news.text}
+                  </Card.Text>
+                  <Link className="ms-auto background-title">Ver más</Link>
+                </Card.Body>
+              </Card>
+            ))}
+          </PagerView>
+        </Container>
+      </Container>
+    </section>
   );
 }
