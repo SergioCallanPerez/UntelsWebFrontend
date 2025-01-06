dac1f3fb90aaae33af5795e62000803b14641c24
JoshuaPariona <joshiparsa@gmail.com>
2025-01-06 00:10:49 -0500

    Add Benefits section with content and styling; update main layout

--- src/main/pages/main/index.jsx
+++ src/main/pages/main/index.jsx
@@ -6,12 +6,14 @@ import Experiences from "./sections/experiences.jsx";
 import Achievements from "./sections/achievements.jsx";
 import AcademicPrograms from "./sections/academic_programs.jsx";
 import CampusLife from "./sections/campus_life.jsx";
+import Benefits from "./sections/benefits.jsx";
 
 export default function Main() {
   return (
     <main className="d-flex flex-column gap-5 pb-5">
       <MainCarousel />
       <Achievements />
+      <Benefits />
       <AcademicPrograms />
       <CampusLife />
       <Announcements />
---
+++ src/main/pages/main/sections/benefits.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Container, Card, Button } from "react-bootstrap";
+import { isMobile } from "react-device-detect";
+import { FaCheckCircle } from "react-icons/fa";
+import benefits_img from "@/assets/benefits.png";
+import "@/styles/benefits.css";
+
+export default function Benefits() {
+
+  return (
+    <section>
+      <Container >
+        <div className="benefits-container">
+        <img src={benefits_img} className="benefits-img" />
+          <Card className="primary benefits-card border-0">
+            <Card.Body className="d-flex m-5 p-0 gap-4 flex-column justify-content-center">
+              <h2 className="with-underline text-center">
+                <b>Construye tu Futuro con Nosotros</b>
+              </h2>
+              <p>
+                En UNTELS encontrarás el lugar perfecto para crecer y alcanzar
+                tus metas profesionales Te ofrecemos :
+              </p>
+              <h5>
+                Te ofrecemos :
+              </h5>
+              <div>
+                <p>
+                  <FaCheckCircle /> Infraestructura moderna: laboratorios,
+                  biblioteca digital y más.
+                </p>
+                <p>
+                  <FaCheckCircle /> Educación de calidad con docentes
+                  especializados.
+                </p>
+                <p>
+                  <FaCheckCircle /> Convenios internacionales para intercambio y
+                  aprendizaje global.
+                </p>
+                <p>
+                  <FaCheckCircle /> Oportunidades de investigación con impacto
+                  social.
+                </p>
+                <p>
+                  <FaCheckCircle /> Una vida universitaria activa e inclusiva.
+                </p>
+              </div>
+              <Button
+                className={`btn-on-primary me-auto px-5 py-2 ${
+                  isMobile ? "mobile" : ""
+                }`}
+              >
+                <b>Conoce más</b>
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
+    </section>
+  );
+}
