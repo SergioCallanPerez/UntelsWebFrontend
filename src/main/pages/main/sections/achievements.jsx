2ffc4482eb0811564a7cf042abe40ba45c0cd537
JoshuaPariona <joshiparsa@gmail.com>
2025-01-05 22:25:15 -0500

    Update SVG colors and refactor Achievements component for improved layout and styling

--- src/main/pages/main/sections/achievements.jsx
+++ src/main/pages/main/sections/achievements.jsx
@@ -6,32 +6,52 @@ import Laboratories from "../../../assets/svgs/laboratories.svg";
 import Agreements from "../../../assets/svgs/handshake.svg";
 
 export default function Achievements() {
-    return (
-        <section className="mt-2 mb-2">
-            <Container>
-                <Row>
-                    <Col md={3} className="d-flex flex-column align-items-center text-center">
-                        <img src={TopTen} height="50px"/>
-                        <h3 className="background-title"><b>Top 10</b></h3>
-                        <p>en Investigación 2024, reconocimiento nacional.</p>
-                    </Col>
-                    <Col md={3} className="d-flex flex-column align-items-center text-center">
-                        <img src={Career} height="50px"/>
-                        <h3 className="background-title"><b>5 carreras</b></h3>
-                        <p>acreditadas y con un alto índice de empleabilidad.</p>
-                    </Col>
-                    <Col md={3} className="d-flex flex-column align-items-center text-center">
-                        <img src={Laboratories} height="50px"/>
-                        <h3 className="background-title"><b>24 laboratorios</b></h3>
-                        <p>equipados para el aprendizaje práctico.</p>
-                    </Col>
-                    <Col md={3} className="d-flex flex-column align-items-center text-center">
-                        <img src={Agreements} height="50px"/>
-                        <h3 className="background-title"><b>Convenio con la UNAB</b></h3>
-                        <p>para fortalecer la educación y la investigación.</p>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    );
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col
+            md={3}
+            className="text-center"
+          >
+            <img src={TopTen}  height="50px" className="mb-3"/>
+            <h4 className="background-title">
+              <b>Top 10</b>
+            </h4>
+            <p>en Investigación 2024, reconocimiento nacional.</p>
+          </Col>
+          <Col
+            md={3}
+            className="text-center"
+          >
+            <img src={Career} height="50px" className="mb-3"/>
+            <h4 className="background-title">
+              <b>5 carreras</b>
+            </h4>
+            <p>acreditadas y con un alto índice de empleabilidad.</p>
+          </Col>
+          <Col
+            md={3}
+            className="text-center"
+          >
+            <img src={Laboratories} height="50px" className="mb-3"/>
+            <h4 className="background-title">
+              <b>24 laboratorios</b>
+            </h4>
+            <p>equipados para el aprendizaje práctico.</p>
+          </Col>
+          <Col
+            md={3}
+            className="text-center"
+          >
+            <img src={Agreements} height="50px" className="mb-3"/>
+            <h4 className="background-title">
+              <b>Convenio con la UNAB</b>
+            </h4>
+            <p>para fortalecer la educación y la investigación.</p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
