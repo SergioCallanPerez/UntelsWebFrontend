8d0e4d11e6e754f025f6055b825a3ca6fca6f880
Franco Antonio Matos Ramos <113787760+FrancoAMR@users.noreply.github.com>
2025-01-06 08:56:10 -0500

    removing some dependences

--- src/main/pages/about-us/organization_chart.jsx
+++ src/main/pages/about-us/organization_chart.jsx
@@ -1,8 +1,6 @@
 import { React } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import "../../styles/root.css";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css"; 
 import organization_chartPDF from "@/assets/pdfs/ORGANIGRAMA2021.pdf";
 import "@/styles/about-us.css";
 
@@ -12,9 +10,7 @@ export default function OrganizationChart(){
             <h3 className="ms-3 about-us-title with-h-divider-on-background">Organigrama</h3>
             <Container>
                 <div style={{ height: "600px" }}>
-                <Worker workerUrl={`./node_modules/pdfjs-dist/build/pdf.worker.min.js`}>
-                    <Viewer fileUrl={organization_chartPDF} />
-                </Worker>
+
 
                 </div>
             </Container>
